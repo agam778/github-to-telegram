c2a972a1b154345a6c299e0938628ba66bdcaf83
Agampreet Singh <agam778singh@outlook.com>
2022-02-09 14:27:45 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# gh-webhooks-tg
+Integrate your GitHub Repo events with Telegram!
+
+# Self Deploy
+- Fork this repo
+- Create a telegram bot at [@BotFather](https://t.me/botfather) (for getting the bot token)
+- Make your bot admin in the channel/chat you are going to connect
+- Add [@MissRose_Bot](https://t.me/MissRose_Bot), make it admin and run `/id` command (to get the id of chat/channel)
+- Make a .env file/fill your environment variables like given in [`.env.sample`](./.env.sample)
+- Install all dependencies - `yarn; yarn install`
+- Run the bot - `yarn start`
+- Now go to your GitHub Repo's settings and add a webhook to your repo, fill the Payload URL with `https://<your-domain>/webhook` and Content Type as `application/json`. Keep the secret blank as it hasn't been implemented yet.
+- Then save your webhook and tada! You are ready to go.
+
+Try Starring your repo/making a commit etc. and you will see the bot sending the message in the chat/channel!
+
+Current events supported:
+- `Stars`
+- `Push`
+- `Forks`
+- `Pull Requests`
+- `Issues`
+- `Releases`
+
+This project is in development and more things are being added!
---
+++ index.js
@@ -0,0 +1,144 @@
+// now we will create a kindof website which we can integrate with github and telegram
+// like we will recieve `/webhook` from github and we will send a message to telegram
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const request = require("request");
+const fs = require("fs");
+const router = express.Router();
+const { Telegraf } = require("telegraf");
+
+if (fs.existsSync(".env")) {
+  dotenv.config();
+}
+
+const app = express();
+const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+async function sendMessage(message) {
+  await bot.telegram.sendMessage(TELEGRAM_CHAT_ID, message, {
+    parse_mode: "markdown",
+    disable_web_page_preview: true,
+  });
+}
+
+router.post("/webhook", (req, res) => {
+  let data = req.body;
+  if (data.starred_at) {
+    sendMessage(
+      `A New star has been added! ⭐\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Stars Count: \`${data.repository.stargazers_count}\``
+    );
+  } else if (data.head_commit) {
+    let commitdate = new Date(data.head_commit.timestamp);
+    let commitdateformatted =
+        commitdate.getDate() +
+        "/" +
+        (commitdate.getMonth() + 1) +
+        "/" +
+        commitdate.getFullYear(),
+      commitmessage = data.head_commit.message;
+    sendMessage(
+      `A New commit has been pushed! 💥\n\n- Repository: [${
+        data.repository.full_name
+      }](${
+        data.repository.html_url
+      })\n- Commit Date: \`${commitdateformatted}\`\n- Commit Message: \`${commitmessage}\`\n- Commit Author: \`${
+        data.head_commit.author.name
+      }\`\n- Commit Branch: \`${
+        data.ref.split("/")[2]
+      }\`\n- Commit URL: [HERE](${data.head_commit.url})`
+    );
+  } else if (data.forkee) {
+    sendMessage(
+      `A New Fork has been created! 🍴\n\n- Forked Repository: [${data.forkee.full_name}](${data.forkee.repository.html_url})\n- Forked From: [${data.repository.full_name}](${data.repository.html_url})`
+    );
+  } else if (data.action && data.issue) {
+    if (data.action === "opened") {
+      sendMessage(
+        `An issue has been opened! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Issue Title: \`${data.issue.title}\`\n- Issue URL: [HERE](${data.issue.html_url})`
+      );
+    } else if (data.action === "closed") {
+      sendMessage(
+        `An issue has been closed! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Issue Title: \`${data.issue.title}\`\n- Issue URL: [HERE](${data.issue.html_url})`
+      );
+    } else if (data.action === "reopened") {
+      sendMessage(
+        `An issue has been reopened! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Issue Title: \`${data.issue.title}\`\n- Issue URL: [HERE](${data.issue.html_url})`
+      );
+    } else if (data.action === "locked") {
+      sendMessage(
+        `An issue has been locked! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Issue Title: \`${data.issue.title}\`\n- Issue URL: [HERE](${data.issue.html_url})`
+      );
+    } else if (data.action === "unlocked") {
+      sendMessage(
+        `An issue has been unlocked! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Issue Title: \`${data.issue.title}\`\n- Issue URL: [HERE](${data.issue.html_url})`
+      );
+    }
+  } else if (data.action && data.pull_request) {
+    if (data.action === "opened") {
+      sendMessage(
+        `A pull request has been opened! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Pull Request Title: \`${data.pull_request.title}\`\n- Pull Request URL: [HERE](${data.pull_request.html_url})`
+      );
+    } else if (data.action === "closed") {
+      sendMessage(
+        `A pull request has been closed/merged! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Pull Request Title: \`${data.pull_request.title}\`\n- Pull Request URL: [HERE](${data.pull_request.html_url})`
+      );
+    } else if (data.action === "locked") {
+      sendMessage(
+        `A pull request has been locked! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Pull Request Title: \`${data.pull_request.title}\`\n- Pull Request URL: [HERE](${data.pull_request.html_url})`
+      );
+    } else if (data.action === "unlocked") {
+      sendMessage(
+        `A pull request has been unlocked! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Pull Request Title: \`${data.pull_request.title}\`\n- Pull Request URL: [HERE](${data.pull_request.html_url})`
+      );
+    } else if (data.action === "reopened") {
+      sendMessage(
+        `A pull request has been reopened! 🐛\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Pull Request Title: \`${data.pull_request.title}\`\n- Pull Request URL: [HERE](${data.pull_request.html_url})`
+      );
+    }
+  } else if (data.action && data.release) {
+    if (data.action === "published") {
+      sendMessage(
+        `A release has been published! 🎉\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "unpublished") {
+      sendMessage(
+        `A release has been unpublished! \n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "created") {
+      sendMessage(
+        `A release has been created! 🎉\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "edited") {
+      sendMessage(
+        `A release has been edited!\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "deleted") {
+      sendMessage(
+        `A release has been deleted!\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "prereleased") {
+      sendMessage(
+        `A release has been prereleased!\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    } else if (data.action === "released") {
+      sendMessage(
+        `A release has been released!\n\n- Repository: [${data.repository.full_name}](${data.repository.html_url})\n- Release Title: \`${data.release.name}\`\n- Release URL: [HERE](${data.release.html_url})`
+      );
+    }
+  }
+});
+
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
