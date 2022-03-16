af4e912e2ab0fb473a346606224f03f29ecfd98b
Agampreet Singh <68941022+agam778@users.noreply.github.com>
2022-03-16 15:55:33 +0530

    Add anti crash

--- index.js
+++ index.js
@@ -169,3 +169,20 @@ app.listen(PORT, (err) => {
   }
   console.log(`Server listening on port ${PORT}`);
 });
+
+process.on("unhandledRejection", (reason, p) => {
+  console.log(" [antiCrash] :: Unhandled Rejection/Catch");
+  console.log(reason, p);
+});
+process.on("uncaughtException", (err, origin) => {
+  console.log(" [antiCrash] :: Uncaught Exception/Catch");
+  console.log(err, origin);
+});
+process.on("uncaughtExceptionMonitor", (err, origin) => {
+  console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
+  console.log(err, origin);
+});
+process.on("multipleResolves", (type, promise, reason) => {
+  console.log(" [antiCrash] :: Multiple Resolves");
+  console.log(type, promise, reason);
+});
